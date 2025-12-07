2d17f1bfda32640a73e620921e0de5e53c47b0d8
doer00kg <160237842+doer00kg@users.noreply.github.com>
2025-12-07 13:45:43 +0200

    data storage w ID generators.py

--- app/data.py
+++ app/data.py
@@ -9,3 +9,21 @@ record_id_counter = 1
 users = {}
 categories = {}
 records = {}
+
+def get_next_user_id():
+    global user_id_counter
+    current_id = user_id_counter
+    user_id_counter += 1
+    return current_id
+
+def get_next_category_id():
+    global category_id_counter
+    current_id = category_id_counter
+    category_id_counter += 1
+    return current_id
+
+def get_next_record_id():
+    global record_id_counter
+    current_id = record_id_counter
+    record_id_counter += 1
+    return current_id
