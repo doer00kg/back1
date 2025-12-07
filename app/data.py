bd1c02c666bfbfd7cd0e34ffebb55ba6586ae4d6
doer00kg <160237842+doer00kg@users.noreply.github.com>
2025-12-07 13:44:46 +0200

    Create data.py

---
+++ app/data.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+# Глобальні лічильники для ID
+user_id_counter = 1
+category_id_counter = 1
+record_id_counter = 1
+
+# Сховища
+users = {}
+categories = {}
+records = {}
